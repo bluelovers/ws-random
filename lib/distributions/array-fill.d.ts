e71ffdc092ac321987350c7f1e83078cb049eec3
bluelovers <codelovers@users.sourceforge.net>
2018-11-30 10:59:15 +0800

    .

--- lib/distributions/array-fill.d.ts
+++ lib/distributions/array-fill.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="node" />
-import { ITSArrayLikeWriteable } from 'ts-type';
+import { IArrayInput02 } from '../type';
 import { Random } from '../random';
-export default function arrayFill(random: Random, min?: number, max?: number, float?: boolean): <T extends ITSArrayLikeWriteable<number> | Float32Array | Float64Array | Int8Array | Int16Array | Int32Array | Uint8ClampedArray | Uint8Array | Uint16Array | Uint32Array | Buffer>(arr: T) => T;
+export default function arrayFill(random: Random, min?: number, max?: number, float?: boolean): <T extends IArrayInput02<number>>(arr: T) => T;
