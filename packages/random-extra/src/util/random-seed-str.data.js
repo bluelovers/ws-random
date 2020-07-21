6b23f58dfd30041e4a3595bff970075e65cf110d
bluelovers <codelovers@users.sourceforge.net>
2020-07-21 11:58:24 +0800

    build: update seed

--- packages/random-extra/src/util/random-seed-str.data.js
+++ packages/random-extra/src/util/random-seed-str.data.js
@@ -1,6 +1,6 @@
 "use strict";
 module.exports = {
     "name": "random-extra",
-    "version": "3.6.11"
+    "version": "3.6.12"
 };
 //# sourceMappingURL=random-seed-str.data.js.map
--- packages/random-extra/src/util/random-seed-str.data.ts
+++ packages/random-extra/src/util/random-seed-str.data.ts
@@ -2,6 +2,6 @@
 
 export = {
 	"name": "random-extra",
-	"version": "3.6.11"
+	"version": "3.6.12"
 }
 
